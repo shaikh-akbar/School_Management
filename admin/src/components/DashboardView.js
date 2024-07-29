8d33ad1162cbc952fd8eca0bb1c8ca74a4a14deb
shaikh-akbar <akbar000785@gmail.com>
2024-07-29 14:57:32 +0530

    search pure

--- admin/src/components/DashboardView.js
+++ admin/src/components/DashboardView.js
@@ -5,19 +5,19 @@ import profile from '../assets/profile.png'
 function DashboardView() {
     return (
         <div className='flex items-center justify-between h-[70px] shadow-lg px-[25px]'>
-            {/* <div className='flex items-center rounded-[5px]'>
+            <div className='items-center rounded-[5px] hidden'>
                 <input type='text' className='bg-[#F8F9FC] h-[40px] outline-none pl-[13px] w-[350px] rounded-[5px] placholder:text-[14px] leading-[20px] font-normal' placeholder='Search For..' />
                 <div className='bg-[#4E73DF] h-[40px] px-[14px] flex items-center justify-center cursor-pointer rounded-tr-[5px]  rounded-br-[5px]'>
                     <FaSearch color='white' />
                 </div>
-            </div> */}
+            </div>
             <div className='flex items-center gap-[15px] relative'>
                 <div className='flex items-center gap-[25px] border-r-[1px] pr-[25px]'>
                     <FaRegBell />
                     <FaEnvelope />
                 </div>
                 <div className='flex items-center gap-[15px] relative'>
-                    <p>Akbar</p>
+                    <p>Admin</p>
                     <div className='h-[50px] w-[50px] flex items-center justify-center relative bg-[#4E73DF]rounded-full'>
                         <img src={profile} alt='' className='h-[40px] w-[40px]'/>
                     </div>
