102043836adddab3071d41acd02492cb8053bd03
shaikh-akbar <akbar000785@gmail.com>
2024-07-29 14:53:16 +0530

    remoivng the main search

--- admin/src/components/DashboardView.js
+++ admin/src/components/DashboardView.js
@@ -5,12 +5,12 @@ import profile from '../assets/profile.png'
 function DashboardView() {
     return (
         <div className='flex items-center justify-between h-[70px] shadow-lg px-[25px]'>
-            <div className='flex items-center rounded-[5px]'>
+            {/* <div className='flex items-center rounded-[5px]'>
                 <input type='text' className='bg-[#F8F9FC] h-[40px] outline-none pl-[13px] w-[350px] rounded-[5px] placholder:text-[14px] leading-[20px] font-normal' placeholder='Search For..' />
                 <div className='bg-[#4E73DF] h-[40px] px-[14px] flex items-center justify-center cursor-pointer rounded-tr-[5px]  rounded-br-[5px]'>
                     <FaSearch color='white' />
                 </div>
-            </div>
+            </div> */}
             <div className='flex items-center gap-[15px] relative'>
                 <div className='flex items-center gap-[25px] border-r-[1px] pr-[25px]'>
                     <FaRegBell />
